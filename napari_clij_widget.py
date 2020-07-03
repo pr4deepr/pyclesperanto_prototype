62ad770a7896872affc2c81d5864bca057abdb5b
Pradeep <pradeep.rajasekhar@gmail.com>
2020-07-04 09:26:22 +1000

    used functools.partial so functions can be used as enum values

--- napari_clij_widget.py
+++ napari_clij_widget.py
@@ -2,50 +2,48 @@ two_dim = "data/blobs.tif"
 three_dim = "data/t1-head.tif"
 
 from skimage import io
-import numpy as np
 import napari
 import clesperanto as cle
-from magicgui import magicgui #https://magicgui.readthedocs.io/en/latest/
+from magicgui import magicgui  # https://magicgui.readthedocs.io/en/latest/
 from napari.layers import Image
 from enum import Enum
+from functools import partial
 
-image_2d = io.imread(two_dim)
+#image_2d = io.imread(two_dim)
 image_3d = io.imread(three_dim)
-
 
 # Using Enums for getting a dropdown menu
 # clesperanto functions are not being passed as enum values for some reason, so they are defined as strings
-class gpu_filter( Enum):
-    def __new__(cls, value, operation):
-        obj = object.__new__(cls)
-        obj.operation = operation
-        return obj
-
+class gpu_filter(Enum):
     # using pyclesperanto filtering images with a gpu
-    mean =          (0, cle.minimum_sphere)
-    maximum =       (0, cle.maximum_sphere)
-    minimum =       (0, cle.minimum_sphere)
-    top_hat =       (0, cle.top_hat_sphere)
-    gaussian_blur = (0, cle.gaussian_blur)
-    crop =          (0, cle.crop)
-                     # todo: get rid of the 0
+    #using functools.partial to return functions as enum values:
+    #https://stackoverflow.com/questions/40338652/how-to-define-enum-values-that-are-functions
+    mean = partial(cle.minimum_sphere)
+    maximum = partial(cle.maximum_sphere)
+    minimum = partial(cle.minimum_sphere)
+    top_hat = partial(cle.top_hat_sphere)
+    gaussian_blur = partial(cle.gaussian_blur)
+    crop = partial(cle.crop)
+
+    #define the call method for the functions or it won't return anything
+    def __call__(self, *args):
+        return self.value(*args)
 
 
 with napari.gui_qt():
     viewer = napari.Viewer()
     viewer.add_image(image_3d, name='3D_image_orig')
-    viewer.add_image(image_2d, name='2D_image_orig')
+    #viewer.add_image(image_2d, name='2D_image_orig')
 
 
     # use auto_call=True for instantaneous execution
-    #can add sliders using QSlider, but need to show values
-    @magicgui(auto_call=True, call_button='Compute')
+    # can add sliders using QSlider, but need to show values
+    @magicgui(auto_call=True)#, call_button='Compute')
     def clij_filter(input: Image, operation: gpu_filter, x: float = 0, y: float = 0,
                     z: float = 0) -> Image:
         if input:
             cle_input = cle.push_zyx(input.data)
-            operation = operation.operation
-
+            operation = operation
             output = cle.create_like(cle_input)
             operation(cle_input, output, x, y, z)
 
